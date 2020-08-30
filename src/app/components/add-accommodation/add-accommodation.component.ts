b8ce857ce24f8c33abdbe8fa43781b3cfaa473f7
tessievh <tessievanhintum@gmail.com>
2020-08-30 18:37:13 +0200

    modals

---
+++ src/app/components/add-accommodation/add-accommodation.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Accommodation } from 'src/app/Model/Accommodation';
+import { AddAccommodationService } from 'src/app/Services/add-accommodation.service';
+
+@Component({
+  selector: 'app-add-accommodation',
+  templateUrl: './add-accommodation.component.html',
+  styleUrls: ['./add-accommodation.component.scss']
+})
+export class AddAccommodationComponent implements OnInit {
+
+  accommodation: Accommodation;
+  accommodationName: string;
+  allRestaurants: Accommodation[];
+
+  constructor(private service: AddAccommodationService ) { }
+
+  ngOnInit(): void {
+  }
+
+
+  getByAccommodationName() {
+    this.service.getByAccommodationName(this.accommodationName).subscribe(
+      data => {
+        this.accommodation = data;
+      }
+    );
+
+}
+
+
+
+}
