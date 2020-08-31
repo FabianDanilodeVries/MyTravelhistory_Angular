5ae15547f862a6e2f0f077ecefe4644081a58079
FabianDanilodeVries <fabiandvrs@hotmail.com>
2020-08-31 14:29:03 +0200

    build

--- AngularMTH/src/app/services/add-accommodation.service.ts
+++ AngularMTH/src/app/services/add-accommodation.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Accommodation } from '../models/Accommodation';
+import { environment } from '../../environments/environment';
 
 
 const httpOptions = {
